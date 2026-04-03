dede5cfcb3531d97da20daab8c226c558f611e0b
musson3d <musson3d@gmail.com>
2026-04-03 17:15:45 +0300

    Create tc-sso.js

---
+++ tc-sso.js
@@ -0,0 +1,11 @@
+var q = window.location.search;
+var s1 = q.indexOf("sso=");
+var sso = null;
+if (s1 > -1) {
+  var s2 = q.indexOf("&", s1);
+  var raw = s2 > -1 ? q.substring(s1+4, s2) : q.substring(s1+4);
+  sso = raw.replace(/\./g, " ");
+}
+window["tolstoycomments"] = window["tolstoycomments"] || [];
+window["tolstoycomments"].push({action:"init", values:{visible:true, sso:sso, show_classic_mobile:0}});
+(function(w,d,s,l,x){w[l]=w[l]||[];w[l].t=w[l].t||new Date().getTime();var f=d.getElementsByTagName(s)[0],j=d.createElement(s);j.async=!0;j.src="//web.tolstoycomments.com/sitejs/app.js?i="+l+"&x="+x+"&t="+w[l].t;f.parentNode.insertBefore(j,f);})(window,document,"script","tolstoycomments","6950");
