58fd4d60165f7c0e0cfe2aaf85dde34d90789948
musson3d <musson3d@gmail.com>
2026-04-03 23:51:03 +0300

    Update tc-sso.js

--- tc-sso.js
+++ tc-sso.js
@@ -6,6 +6,6 @@ if (s1 > -1) {
   var raw = s2 > -1 ? q.substring(s1+4, s2) : q.substring(s1+4);
   sso = raw.replace(/\./g, " ");
 }
-
 window["tolstoycomments"] = window["tolstoycomments"] || [];
 window["tolstoycomments"].push({action:"init", values:{visible:true, sso:sso, show_classic_mobile:0}});
+(function(w,d,s,l,x){w[l]=w[l]||[];w[l].t=w[l].t||new Date().getTime();var f=d.getElementsByTagName(s)[0],j=d.createElement(s);j.async=true;j.src="https://cackle.me/tc/widget.js";f.parentNode.insertBefore(j,f);})(window,document,"script","tolstoycomments","x");
